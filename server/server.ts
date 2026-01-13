6a1ece8d4a00bb7be4a7abab04a11967f8bd3ee2
harshrautela9100@gmail.com <harshrautela9100@gmail.com>
2026-01-13 21:38:25 +0530

    last

--- server/server.ts
+++ server/server.ts
@@ -17,7 +17,7 @@ await connectionDB();
 const app=  express()
 const port =process.env.PORT || 3000;
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:3000'],
+    origin:['http://localhost:5173','http://localhost:3000',"https://thumbify-rho.vercel.app/login"],
     credentials:true,
 }))
 app.use(session({
