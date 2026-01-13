84e49cf06453b1a474f22e2bc259b07e5cd00729
harshrautela9100@gmail.com <harshrautela9100@gmail.com>
2026-01-13 21:07:43 +0530

    final

--- server/controlllers/UserController.ts
+++ server/controlllers/UserController.ts
@@ -3,8 +3,8 @@ import Thumbnail from '../models/Thumbnail.js';
 export const getUsersThumbnails = async (req:Request,res:Response)=>{
     try{
         const {userId}=req.session;
-    const thumbnail = await Thumbnail.find({userId}).sort({createdAt:-1});
-    res.json({thumbnail});
+    const thumbnails = await Thumbnail.find({userId}).sort({createdAt:-1});
+    res.json({thumbnails});
 
     } catch(error:any){
         res.json({
